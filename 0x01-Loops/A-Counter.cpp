135a588d354ba0c4635dfe7611ac06cfcc781598
foash-111 <mr2221@fayoum.edu.eg>
2025-01-06 14:30:57 +0200

    simple counter loop

---
+++ 0x01-Loops/A-Counter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+
+    for(int i = 0; i < x; i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
