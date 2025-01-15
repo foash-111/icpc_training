470a46dcc842c4460dc62ce9d90a26e14a0bceae
foash-111 <mr2221@fayoum.edu.eg>
2025-01-15 12:23:16 +0200

    E - Maximum Multiple Sum

---
+++ 0x01-Loops/E-Maximum_Multiple_Sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int x, num, j = 2;
+    int max_sum = 0, temp = 0, max_multible = 0;
+    cin>>x;
+    while (x--)
+    {
+        cin>>num;
+
+       num == 3 ? max_multible = 3 : max_multible = 2;
+    
+    cout<<max_multible<<endl;
+    }
+}
