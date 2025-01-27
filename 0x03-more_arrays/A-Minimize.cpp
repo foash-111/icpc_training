1659bc260d1ee6b76ccca107a3eb88f9e7d9da52
foash-111 <mr2221@fayoum.edu.eg>
2025-01-27 23:39:09 +0200

    A - Minimize

---
+++ 0x03-more_arrays/A-Minimize.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x; cin>>x;
+    int a, b;
+    while(x--){
+        cin>>a>>b;
+        cout<< b - a <<endl;
+    }
+    return 0;
+}
